8c36c3ba91a37e8633077ad047e5eb04ff371f76
COMPUTER\Sergey Ze352 <zeliksv@gmail.com>
2018-11-24 18:24:32 +0300

    Start changes less5

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -5,34 +5,31 @@ import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
 // Components
+
+import Spinner from '../Spinner';
 import StatusBar from '../StatusBar';
 import Composer from '../Composer';
 import Post from '../Post';
 
-
-export default class Feed extends Component{
+export default class Feed extends Component {
         state = {
-            posts: [
-                {
-                    id: "123",
-                    comment: 'Hi there Bart!'
-                },
-                {
-                    id: "456"
-                }
-            ],
-    }
-    render(){
-        const { posts } = this.state;
+            posts: [{ id: "123", comment: 'Hi there Bart!', created: 1523708450 }, { id: "456", comment: 'I m fine. And how are you?', created: 1523708756 } ],
+            posting: false,
+        }
+    render () {
+
+        const { posts, posting } = this.state;
         const postJSX = posts.map((post) => {
             return <Post key = { post.id } { ...post } />;
-        })
+        });
+
         return(
                 <section className = { Styles.feed }>
+                    <Spinner isSpinning = { posting }/>
                     <StatusBar />
                     <Composer />
                     { postJSX }
                 </section>
-        )
+        );
     }
 }
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,14 +1,21 @@
-
+// Core
 import React, { Component } from 'react';
 import { Consumer } from '../HOC/withProfile';
+import PropTypes from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
 import moment from 'moment';
 
 export default class Post extends Component{
-    render(){
-        const { comment } = this.props;
+    
+    static propTypes = {
+        comment: PropTypes.string.isRequired,
+        created: PropTypes.number.isRequired,
+    };
+
+    render () {
+        const { comment, created } = this.props;
 
         return(
          <Consumer>
@@ -18,11 +25,10 @@ export default class Post extends Component{
                          <section className={Styles.post}>
                              <img src={ context.avatar }/>
                              <a>{` ${ context.currentUserFirstName } ${ context.currentUserLastName } `}</a>
-                             <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                             <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
                              <p>{ comment }</p>
                          </section>
-                     )
-
+                        )
              }
          </Consumer>
 
